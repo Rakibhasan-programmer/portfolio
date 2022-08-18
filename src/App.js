71becb890c4daa3ac5d505e6d1220238295bdb87
unknown <rakibulhasan.seu.cse@gmail.com>
2022-08-19 04:35:42 +0600

    more components added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Experience from "./components/Experience";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import Portfolio from "./components/Portfolio";
 import SocialLinks from "./components/SocialLinks";
 
 
@@ -9,6 +13,10 @@ function App() {
       <Navbar />
       <Home />
       <SocialLinks />
+      <About />
+      <Portfolio />
+      <Experience />
+      <Contact />
     </div>
   );
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+        <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
+            </div>
+            <p className='text-xl mt-20'>My name is Rakibul Hasan. I am from Naogaon. Currently I am an undergraduate at Southeast University. I am a positive, enthusiastic and competent Web Developer who, over the years, has built up a diverse range of skills, qualities and attributes that guarantee I will perform highly in this role. Beside this I love problem solving. I participate in different problem solving contests.</p>
+            <br />
+            <p className="text-xl">
+                Currently I am exploring the web with the help of technologies like JavaScript, React, Tailwind css, Next js. I want to explore more on this field as I am getting more interest day by day. I love to face Challenges.
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default About
