bfd07943c2f0c1a4a1360cd7ad4f63fec5738384
unknown <rakibulhasan.seu.cse@gmail.com>
2022-08-24 23:27:55 +0600

    animation off

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-        <div className='bg-gradient-to-b from-gray-800 to-gray-800'>
+        <div className='bg-gradient-to-b from-gray-800 to-gray-800 w-full'>
            <p className='font-semibold text-gray-400 py-4 text-center'>&copy; 2022 Md Rakibul Hasan - All rights reserved</p>
         </div>
     </div>
